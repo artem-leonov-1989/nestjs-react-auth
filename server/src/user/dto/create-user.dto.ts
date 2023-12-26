d1a290dc8399a8e78a7b83c1cec12c834f494ecc
Artem Leonov <artem_leonov@hotmail.com>
2023-12-26 16:36:39 +0200

    Add user registration.

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -1,5 +1,14 @@
+import {IsString, MinLength} from "class-validator";
+
 export class CreateUserDto {
+    @IsString()
+    @MinLength(3, {message: 'Name must be more than 3 characters'})
     username: string;
+
+    @IsString()
+    @MinLength(2, {message: 'Name must be more than 3 characters'})
     name: string;
+
+    @MinLength(6, {message: 'Password must be more than 6 characters'})
     password: string;
 }
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,26 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import {BadRequestException, Injectable} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import {InjectRepository} from "@nestjs/typeorm";
+import {User} from "./entities/user.entity";
+import {Repository} from "typeorm";
+import * as argon2 from "argon2";
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
-
-  findAll() {
-    return `This action returns all user`;
+  constructor(
+      @InjectRepository(User) private readonly userRepository: Repository<User>
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    const existUser = await this.userRepository.findOne({
+      where: {
+        username: createUserDto.username
+      }
+    })
+    if (existUser) throw new BadRequestException('This username already exist!')
+    const  user = await this.userRepository.save({
+      username: createUserDto.username,
+      name: createUserDto.name,
+      password: await argon2.hash(createUserDto.password)
+    })
+    return { user };
   }
 
   findOne(id: number) {
     return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
   }
 }
