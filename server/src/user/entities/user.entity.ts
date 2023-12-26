316c8221998bf1a71fbaa54ad38125583283b8d3
Artem Leonov <artem_leonov@hotmail.com>
2023-12-26 15:31:57 +0200

    Create User entity.

--- server/src/user/entities/user.entity.ts
+++ server/src/user/entities/user.entity.ts
@@ -1,5 +1,23 @@
+import {Column, CreateDateColumn, Entity, Index, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
+
+@Entity()
 export class User {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    @Index({ unique: true })
     username: string;
+
+    @Column()
     name: string;
+
+    @Column()
     password: string;
+
+    @CreateDateColumn()
+    created_at: Date
+
+    @UpdateDateColumn()
+    updated_at: Date
 }
